3cecfeb111bd972783f37dea29c62b48fe80fdd9
Weafre <doandat.nguyen@gmail.com>
2023-02-04 19:07:52 -0800

    updat

---
+++ LossyPredGeo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+dirs=( "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/Static_Objects_and_Scenes/Cat1_quantized/Shiva_00035_vox12.ply"   "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/Static_Objects_and_Scenes/Cat1_quantized/Frog_00067_vox12.ply"  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/Static_Objects_and_Scenes/Cat1_quantized/House_without_roof_00057_vox12.ply"  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/Static_Objects_and_Scenes/Cat1_quantized/Arco_Valentino_Dense_vox12.ply"  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/Static_Objects_and_Scenes/Cat1_quantized/Egyptian_mask_vox12.ply"  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/Static_Objects_and_Scenes/Cat1_quantized/Facade_00009_vox12.ply"      )
+
+#/home/datnguyen/Projects/mpeg-pcc-tmc13v14/cfg_multiQP/octree-raht/lossy-geom-lossy-attrs
+
+cfgs=("/home/datnguyen/Projects/mpeg-pcc-tmc13v14/cfg_multiQP/predgeom-raht/lossy-geom-lossy-attrs/shiva_00035_vox12"   "/home/datnguyen/Projects/mpeg-pcc-tmc13v14/cfg_multiQP/predgeom-raht/lossy-geom-lossy-attrs/frog_00067_vox12"  "/home/datnguyen/Projects/mpeg-pcc-tmc13v14/cfg_multiQP/predgeom-raht/lossy-geom-lossy-attrs/house_without_roof_00057_vox12"  "/home/datnguyen/Projects/mpeg-pcc-tmc13v14/cfg_multiQP/predgeom-raht/lossy-geom-lossy-attrs/arco_valentino_dense_vox12"  "/home/datnguyen/Projects/mpeg-pcc-tmc13v14/cfg_multiQP/predgeom-raht/lossy-geom-lossy-attrs/egyptian_mask_vox12"  "/home/datnguyen/Projects/mpeg-pcc-tmc13v14/cfg_multiQP/predgeom-raht/lossy-geom-lossy-attrs/facade_00009_vox12"    )
+
+
+dirs=(  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/Static_Objects_and_Scenes/Cat1_quantized/Arco_Valentino_Dense_vox12.ply"  )
+
+
+
+rates=("/r01" "/r02" "/r03" "/r04" "/r05" "/r06" "/r07" "/r08" "/r09" "/r10" "/r11" "/r12" "/r13" "/r14" "/r15"  )
+
+
+{
+for idx in "${!dirs[@]}";
+do
+	for ridx in {0..14}; do
+	scripts/collect-tmc13.pl  "${cfgs[$idx]}${rates[$ridx]}" ${pcs[$idx]}  0   "/home/datnguyen/Projects/mpeg-pcc-tmc13v14/OutputPredGeo${rates[$ridx]}/${pcs[$idx]}"  ${dirs[$idx]}
+	done
+done
+}> TestPredGeo.txt
+
