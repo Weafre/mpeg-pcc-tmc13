076a471378acbbc8e6ac6e390c555bf548129acb
weafre <doandat.nguyen@gmail.com>
2024-02-03 14:57:29 +0100

    update

--- LosslessGeoLossyAttribute.sh
+++ LosslessGeoLossyAttribute.sh
@@ -5,8 +5,9 @@ cfg="/home/datnguyen/Projects/MPEG/tmc13-v14/cfg_attribute/octree-raht/lossless-
 
 #dir="/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MPEG8i/loot/Ply"
 
-test_dirs=("/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MPEG8i/loot/Ply"  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MPEG8i/redandblack/Ply"   " /home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MVUB/phil10/ply"  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MVUB/ricardo10/ply"   "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/Owlii/basketball_vox10"   "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/Owlii/dancer_vox10" )
-pc_names=("loot10" "redandblack10" "phil10" "ricardo10" "basketball_vox10" "dancer_vox10")
+test_dirs=("/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MPEG8i/loot/Ply"  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MPEG8i/redandblack/Ply"   " /home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MVUB/phil10/ply"  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MVUB/ricardo10/ply"    "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TrainPCs/MVUB_10bits/andrew10/ply/" "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TrainPCs/MVUB_10bits/david10/ply/"  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TrainPCs/MVUB_10bits/sarah10/ply/"  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TrainPCs/MPEG_8i_10bit/longdress/Ply/" "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TrainPCs/MPEG_8i_10bit/soldier/Ply/"  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/Owlii/basketball_vox10"   "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/Owlii/dancer_vox10" )
+
+pc_names=("loot10" "redandblack10" "phil10" "ricardo10" "andrew10" "david10" "sarah10" "longdress10" "soldier10" "basketball_vox10" "dancer_vox10")
 
 
 #test_dirs=( "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MPEG8i/loot/Ply"   )
@@ -23,19 +24,20 @@ do
   basename=$(basename ${entry})
   names+=($basename)
 done
-echo $pcs 
+#echo $pcs
 #echo $dir
-mkdir "LossyAttribute/no_pred_${pc_names[$dir_idx]}"
+mkdir "conf2023/${pc_names[$dir_idx]}"
 
 for ridx in {0..5}; do
-mkdir "LossyAttribute/no_pred_${pc_names[$dir_idx]}${rates[$ridx]}"
+mkdir "conf2023/${pc_names[$dir_idx]}${rates[$ridx]}"
 for idx in "${!pcs[@]}";
 do
-  make -f  $PWD/scripts/Makefile.tmc13-step -C "LossyAttribute/no_pred_${pc_names[$dir_idx]}${rates[$ridx]}"  VPATH="${cfg}${rates[$ridx]}"  ENCODER=$PWD/build/tmc3/tmc3  DECODER=$PWD/build/tmc3/tmc3  SRCSEQ=${pcs[$idx]}  VERBOSE=1 PCERROR=/home/datnguyen/Projects/MPEG/mpeg-pcc-dmetric-master/test/pc_error &
-  if (( $idx % 15 == 0 )); then wait; fi
+  make -f  $PWD/scripts/Makefile.tmc13-step -C "conf2023/${pc_names[$dir_idx]}${rates[$ridx]}"  VPATH="${cfg}${rates[$ridx]}"  ENCODER=$PWD/build/tmc3/tmc3  DECODER=$PWD/build/tmc3/tmc3  SRCSEQ=${pcs[$idx]}  VERBOSE=1 PCERROR=/home/datnguyen/Projects/MPEG/mpeg-pcc-dmetric-master/test/pc_error &
+  if (( $idx % 14 == 0 )); then wait; fi
 done
 done
 wait
+find conf2023/ -name "*.ply" -type f -delete
 
 
 {
@@ -43,10 +45,10 @@ for ridx in {0..5}; do
 for idx in "${!pcs[@]}";
 do
   #scripts/collect-tmc13.pl  "${cfg}${rates[$ridx]}" ${names[$idx]}  0   "/home/datnguyen/Projects/MPEG/tmc13-v14/LossyAttribute${rates[$ridx]}/${names[$idx]}"  ${pcs[$idx]}
-   scripts/collect-tmc13-dyna.pl  "${cfg}${rates[$ridx]}" ${names[$idx]}  0   "/home/datnguyen/Projects/MPEG/tmc13-v14/LossyAttribute/no_pred_${pc_names[$dir_idx]}${rates[$ridx]}/${names[$idx]}"  ${pcs[$idx]}
+   scripts/collect-tmc13-dyna.pl  "${cfg}${rates[$ridx]}" ${names[$idx]}  0   "/home/datnguyen/Projects/MPEG/tmc13-v14/conf2023/${pc_names[$dir_idx]}${rates[$ridx]}/${names[$idx]}"  ${pcs[$idx]}
   done
 done
-}  > "LossyAttribute/no_pred_${pc_names[$dir_idx]}.txt"
+}  > "conf2023/gpcc_${pc_names[$dir_idx]}.txt"
 
 done
 
--- reading_gpcc_enc_data.sh
+++ reading_gpcc_enc_data.sh
@@ -5,9 +5,16 @@ cfg="/home/datnguyen/Projects/MPEG/tmc13-v14/cfg_attribute/octree-raht/lossless-
 
 #dir="/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MPEG8i/loot/Ply"
 
-test_dirs=("/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MPEG8i/redandblack/Ply"   " /home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MVUB/phil10/ply"  "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MVUB/ricardo10/ply"   "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/Owlii/basketball_vox10"   "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/Owlii/dancer_vox10" )
+test_dirs=("/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TrainPCs/MPEG_8i_10bit/soldier/Ply/"  )
 
-for dir in "${test_dirs[@]}"; do
+pc_names=( "soldier10")
+
+
+#test_dirs=( "/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/10bits_TestPCs/MPEG8i/loot/Ply"   )
+#pc_names=( "loot10" )
+
+for dir_idx in "${!test_dirs[@]}"; do
+dir=${test_dirs[$dir_idx]}
 pcs=()
 names=()
 
@@ -17,23 +24,45 @@ do
   basename=$(basename ${entry})
   names+=($basename)
 done
-#echo $pcs 
+#echo $pcs
 #echo $dir
+mkdir "conf2023/${pc_names[$dir_idx]}"
+
+for ridx in {0..5}; do
+mkdir "conf2023/${pc_names[$dir_idx]}${rates[$ridx]}"
+for idx in "${!pcs[@]}";
+do
+  make -f  $PWD/scripts/Makefile.tmc13-step -C "conf2023/${pc_names[$dir_idx]}${rates[$ridx]}"  VPATH="${cfg}${rates[$ridx]}"  ENCODER=$PWD/build/tmc3/tmc3  DECODER=$PWD/build/tmc3/tmc3  SRCSEQ=${pcs[$idx]}  VERBOSE=1 PCERROR=/home/datnguyen/Projects/MPEG/mpeg-pcc-dmetric-master/test/pc_error &
+  if (( $idx % 14 == 0 )); then wait; fi
+done
+done
+wait
+find conf2023/ -name "*.ply" -type f -delete
+
 
 {
 for ridx in {0..5}; do
 for idx in "${!pcs[@]}";
 do
   #scripts/collect-tmc13.pl  "${cfg}${rates[$ridx]}" ${names[$idx]}  0   "/home/datnguyen/Projects/MPEG/tmc13-v14/LossyAttribute${rates[$ridx]}/${names[$idx]}"  ${pcs[$idx]}
-   scripts/collect-tmc13-dyna.pl  "${cfg}${rates[$ridx]}" ${names[$idx]}  0   "/home/datnguyen/Projects/MPEG/tmc13-v14/LossyAttribute${rates[$ridx]}/${names[$idx]}"  ${pcs[$idx]}
+   scripts/collect-tmc13-dyna.pl  "${cfg}${rates[$ridx]}" ${names[$idx]}  0   "/home/datnguyen/Projects/MPEG/tmc13-v14/conf2023/${pc_names[$dir_idx]}${rates[$ridx]}/${names[$idx]}"  ${pcs[$idx]}
   done
 done
-}  > "LossyAttribute/${names[-1]}.txt"
+}  > "conf2023/gpcc_${pc_names[$dir_idx]}.txt"
 
 done
 
 
 # make -f  $PWD/scripts/Makefile.tmc13-step -C LossyAttribute/r01/  VPATH=/home/datnguyen/Projects/MPEG/tmc13-v14/cfg_allQP/trisoup-predlift/lossy-geom-lossy-attrs/arco_valentino_dense_vox12/r01/  ENCODER=$PWD/build/tmc3/tmc3  DECODER=$PWD/build/tmc3/tmc3  SRCSEQ="/home/datnguyen/Projects/Datasets/MPEG_MVUB_CAT_9_12bits/Static_Objects_and_Scenes/Cat1_quantized/Arco_Valentino_Dense_vox12.ply"  VERBOSE=1 PCERROR=/home/datnguyen/Projects/MPEG/mpeg-pcc-dmetric-master/test/pc_error
+
+
+
+
+
+
+
+
+
 
 
 
